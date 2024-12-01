297c85293053cdfd027ea0e3eebcf45101e387a2
smahout <me@smahout..nl>
2024-12-01 09:52:08 +0100

    Make int when adding to the list

--- 1/main.py
+++ 1/main.py
@@ -3,18 +3,18 @@ with open("input.txt") as f:
     for line in f:
         line = line.replace("\n", "" )
         pair = line.split("   ")
-        left.append(pair[0])
-        right.append(pair[1])
+        left.append(int(pair[0]))
+        right.append(int(pair[1]))
 
 left = sorted(left)
 right = sorted(right)
 
 distance = 0
 for i in range(len(left)):
-    distance += abs(int(left[i]) - int(right[i]))
+    distance += abs(left[i] - right[i])
 print("Distance:", distance)
 
 product_sum = 0
 for i in range(len(left)):
-    product_sum += right.count(left[i]) * int(left[i])
+    product_sum += right.count(left[i]) * left[i]
 print("Product sum:", product_sum)
