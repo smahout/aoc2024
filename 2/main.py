699155460def12c6bfbaabdb76bb04fde0e2ffc5
smahout <me@smahout..nl>
2024-12-02 14:26:03 +0100

    Advent of code 2024 day 2; Made use of recursion, so we dont unnecessarily try all options. Now we only try up to 4 different options, regardless of how many readings the input has.

--- 2/main.py
+++ 2/main.py
@@ -1,4 +1,8 @@
+import logging
 from copy import copy
+
+debug = logging.debug
+info = logging.info
 
 def is_report_save(report):
     previous = None
@@ -19,6 +23,49 @@ def is_report_save(report):
         previous = reading
     return True
 
+def is_report_saveish(report, try_again=False):
+    increasing = None
+    is_safe = True
+    false_index = None
+    for i in range(len(report)):
+        if i == 0:
+            continue
+        previous = report[i-1]
+        reading = report[i]
+        increasing = reading > previous if increasing is None else increasing
+        if abs(reading - previous) > 3 or abs(reading - previous) == 0:
+            debug(f"Problem with DIFFERENCE; {reading} and {previous}, diff is {abs(reading-previous)}. Try again is {try_again}")
+            is_safe = False
+            false_index = i
+            break
+        if increasing and reading < previous:
+            debug(f"Problem with DECREASE; {reading} and {previous}. Try again is {try_again}")
+            is_safe = False
+            false_index = i
+            break
+        elif not increasing and reading > previous:
+            debug(f"Problem with INCREASE; {reading} and {previous}. Try again is {try_again}")
+            is_safe = False
+            false_index = i
+            break
+
+    if (not is_safe) and try_again:
+        split1 = copy(report)
+        split2 = copy(report)
+        split1.pop(false_index - 1)
+        split2.pop(false_index)
+
+        if false_index - 1 == 1:
+            split0 = copy(report)
+            split0.pop(0)
+            debug(f"Making split1: {split1}. Making split2: {split2}. FIRST PROBLEM ENCOUNTERED ON INDEX ONE. Making split0: {split0}. From source: {report}")
+            return is_report_saveish(split1) or is_report_saveish(split2) or is_report_saveish(split0)
+
+        debug(f"Making split1: {split1}. Making split2: {split2}. From source: {report}")
+        return is_report_saveish(split1) or is_report_saveish(split2)
+    return is_safe
+
+logging.getLogger().setLevel(logging.INFO)
 reports = []
 with open("input_big.txt") as f:
     for line in f:
@@ -27,17 +74,19 @@ with open("input_big.txt") as f:
         reports.append(report)
 
 save = 0
+report_nodamp = []
 for report in reports:
     if is_report_save(report):
+        report_nodamp.append(report)
         save += 1
-print(f"Part one, no problem dampener: {save}")
 
+info(f"Part one, no problem dampener: {save}")
 save = 0
+reports_good = []
 for report in reports:
-    for i in range(len(report)):
-        short_report = copy(report)
-        short_report.pop(i)
-        if is_report_save(short_report):
-            save += 1
-            break
-print(f"Part two, with problem dampener: {save}")
+    debug(f"_____________________Checking report {report}________________________")
+    if result := is_report_saveish(report, True):
+        reports_good.append(report)
+        save += 1
+    debug(f"______________________________Result is {result}____________________________________________")
+info(f"Part two, with problem dampener: {save}")
