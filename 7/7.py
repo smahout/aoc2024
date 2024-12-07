432035e2121bf849228cf76f36a7e6ca0a49db0d
smahout <me@smahout..nl>
2024-12-07 11:04:54 +0100

    Day 7

---
+++ 7/7.py
@@ -0,0 +1,23 @@
+from copy import deepcopy
+from operator import concat
+
+def math(input, intermediate, result):
+    int_mult = 1 if intermediate is None else intermediate
+    int_plus = 0 if intermediate is None else intermediate
+    concatination = 0
+    if len(input) > 0:
+        v = input.pop()
+        if intermediate is not None:
+            concatination = math(deepcopy(input), int(concat(str(intermediate), str(v))), result)
+        return max(math(deepcopy(input), v * int_mult, result), math(deepcopy(input), v + int_plus, result), concatination)
+    elif intermediate == result:
+        return result
+    else:
+        return 0
+
+equations = {int(l.split(":")[0]): list(map(int, l.split(":")[1].split())) for l in open("big7.txt")}
+sum = 0
+print()
+for result in equations:
+    sum += math(equations[result][::-1], None, result)
+print(sum)
