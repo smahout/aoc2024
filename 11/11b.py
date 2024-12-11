a1c08cbd1d9470b22ad6fd55ef8520f834ed1ff4
smahout <me@smahout.nl>
2024-12-11 12:08:21 +0100

    added unit for time

--- 11/11b.py
+++ 11/11b.py
@@ -25,5 +25,5 @@ def part2():
     print("Part 2:", sum([blink(int(stone), 75) for stone in open("big.txt").read().strip().split()]))
 
 
-print("Part 1 runtime: ", timeit.timeit("part1()", "from __main__ import part1", number=1) * 1000)
-print("Part 2 runtime: ", timeit.timeit("part2()", "from __main__ import part2", number=1) * 1000)
+print("Part 1 runtime:", timeit.timeit("part1()", "from __main__ import part1", number=1) * 1000, "ms")
+print("Part 2 runtime:", timeit.timeit("part2()", "from __main__ import part2", number=1) * 1000, "ms")
