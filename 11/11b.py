4035b6623e21f76066db4edf809d99967a89f17a
smahout <me@smahout.nl>
2024-12-11 11:58:25 +0100

    Didnt know about cache decorator, this is cool

---
+++ 11/11b.py
@@ -0,0 +1,17 @@
+from functools import cache
+
+@cache
+def blink(stone, times):
+    if times == 0:
+        return 1
+    times -= 1
+
+    if stone == 0:
+        return blink(1, times)
+    if len(str(stone)) % 2 == 0:
+        left = int(str(stone)[:int(len(str(stone))/2)])
+        right = int(str(stone)[int(len(str(stone))/2):])
+        return blink(left, times) + blink(right, times)
+    return blink(stone * 2024, times)
+
+print(sum([blink(int(stone), 75) for stone in open("big.txt").read().strip().split()]))
