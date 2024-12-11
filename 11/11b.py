36d393c95400a0cf4befcef0fe9d8198d2b9465e
smahout <me@smahout.nl>
2024-12-11 11:59:51 +0100

    pep

--- 11/11b.py
+++ 11/11b.py
@@ -1,5 +1,6 @@
 from functools import cache
 
+
 @cache
 def blink(stone, times):
     if times == 0:
@@ -9,9 +10,10 @@ def blink(stone, times):
     if stone == 0:
         return blink(1, times)
     if len(str(stone)) % 2 == 0:
-        left = int(str(stone)[:int(len(str(stone))/2)])
-        right = int(str(stone)[int(len(str(stone))/2):])
+        left = int(str(stone)[:int(len(str(stone)) / 2)])
+        right = int(str(stone)[int(len(str(stone)) / 2):])
         return blink(left, times) + blink(right, times)
     return blink(stone * 2024, times)
+
 
 print(sum([blink(int(stone), 75) for stone in open("big.txt").read().strip().split()]))
