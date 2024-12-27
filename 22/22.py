dba380591c6371e62c26398f6419fbab8520d52f
smahout <me@smahout.nl>
2024-12-27 01:31:09 +0100

    added timeit

--- 22/22.py
+++ 22/22.py
@@ -1,3 +1,5 @@
+import timeit
+from functools import cache
 from math import floor
 
 
@@ -26,11 +28,19 @@ def changes(evolution_no: int, subset: list, sequences: dict):
         sequences[sequence][evolution_no] = subset[4] % 10
 
 
+def part1(evolutions):
+    for i, secret in enumerate(evolutions):
+        evolutions[i] = evolve(secret, 2000)
+    print(f"Part 1: {sum([e[2000] for e in evolutions])}")
+
+
+def part2(evolutions):
+    sequences = {}
+    [changes(evolution_no, evo[i:i + 5], sequences) for evolution_no, evo in enumerate(evolutions) for i in range(len(evo) - 4)]
+    print(f"Part 2: {max([sum(seq.values()) for seq in sequences.values()])}")
+
+
 with open("big.txt", "r") as f:
-    evolutions = [evolve(int(line), 2000) for line in f.readlines()]
-
-
-print(f"Part 1: {sum([e[2000] for e in evolutions])}")
-sequences = {}
-[changes(evolution_no, evo[i:i+5], sequences) for evolution_no, evo in enumerate(evolutions) for i in range(len(evo) - 4)]
-print(f"Part 2: {max([sum(seq.values()) for seq in sequences.values()])}")
+    evolutions = [int(line) for line in f.readlines()]
+print(f"Part 1 execution time: {timeit.timeit(lambda: part1(evolutions), number=1)}s")
+print(f"Part 2 execution time: {timeit.timeit(lambda: part2(evolutions), number=1)}s")
